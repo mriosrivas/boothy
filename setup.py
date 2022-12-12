79cf43f6fe306b6ad9497bd6503c694553b23191
Manuel <mriosrivas@gmail.com>
2022-12-12 17:06:17 -0600

    Add long description in setup.py.

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 from setuptools import setup
 
 setup(name="boothy",
-      version="0.1",
+      version="0.1.1",
       description="Bootstrap hypothesis testing",
+      long_description="Boothy is an Open Source bootstrap hypothesis library \
+      that can help you evaluate different types of hypothesis tests. The aim \
+      of boothy is to give an easy way to perform an hypothesis test and \
+      interpretation of results.",
       packages=['boothy'],
       zip_safe=False)
